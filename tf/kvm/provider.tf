305e70db42faaaab559bf608301fec0b61093271
trickywork <jl16053@nyu.edu>
2026-04-22 21:00:57 -0400

    Use OpenStack environment auth

--- tf/kvm/provider.tf
+++ tf/kvm/provider.tf
@@ -1,3 +1,6 @@
-provider "openstack" {
-  cloud = "openstack"
-}
+# Leave provider configuration empty so Terraform can use the OpenStack
+# credentials already present in the Chameleon Jupyter environment, including
+# OS_AUTH_URL, OS_AUTH_TYPE, OS_ACCESS_TOKEN, OS_PROJECT_NAME, and OS_REGION_NAME.
+# If a team member wants to use clouds.yaml instead, they can set OS_CLOUD in
+# their shell without changing this file.
+provider "openstack" {}
